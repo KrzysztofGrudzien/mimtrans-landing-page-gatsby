4945315b2283d7b9123037d741c03893924a262b
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-10-12 20:47:03 +0200

    add missing pages

---
+++ src/pages/rozklad-jazdy.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const Timetable = () => {
+  return (
+    <>
+      <Layout>
+        <div>Rozkład jazdy</div>
+      </Layout>
+    </>
+  )
+}
+
+export default Timetable
---
+++ src/pages/taryfa-biletowa.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const TicketPrice = () => {
+  return (
+    <>
+      <Layout>
+        <div>Taryfa Biletowa</div>
+      </Layout>
+    </>
+  )
+}
+
+export default TicketPrice
