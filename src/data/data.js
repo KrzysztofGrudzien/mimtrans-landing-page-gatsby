060d00a0a887bb51a53532b147a6154e8524140a
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-10-13 21:36:48 +0200

    create data file to store different data

---
+++ src/data/data.js
@@ -0,0 +1,54 @@
+const data = {
+  navigation: [
+    {
+      path: "/",
+      title: "Home",
+      className: "navbar-item",
+      linkClassName: "navbar-link",
+    },
+    {
+      path: "/onas",
+      title: "O nas",
+      className: "navbar-item",
+      linkClassName: "navbar-link",
+    },
+    {
+      path: "/uslugi",
+      title: "Usługi",
+      className: "navbar-item",
+      linkClassName: "navbar-link",
+    },
+    {
+      path: "/partnerzy",
+      title: "Partnerzy",
+      className: "navbar-item",
+      linkClassName: "navbar-link",
+    },
+    {
+      path: "/rekomendacje",
+      title: "Rekomendacje",
+      className: "navbar-item ",
+      linkClassName: "navbar-link",
+    },
+    {
+      path: "/kontakt",
+      title: "Kontakt",
+      className: "navbar-item",
+      linkClassName: "navbar-link",
+    },
+    {
+      path: "/rozklad-jazdy",
+      title: "Rozkład jazdy",
+      className: "navbar-item navbar-item--action",
+      linkClassName: "navbar-link",
+    },
+    {
+      path: "/taryfa-biletowa",
+      title: "Taryfa biletowa",
+      className: "navbar-item navbar-item--action",
+      linkClassName: "navbar-link",
+    },
+  ],
+}
+
+export default data
