8a6faa32e6acd888190218edad170bc895fb12a7
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-11-04 19:00:31 +0100

    add a content to the page rekomendacje

--- src/pages/rekomendacje.js
+++ src/pages/rekomendacje.js
@@ -1,11 +1,90 @@
 import React from "react"
 import Layout from "../components/Layout"
+import Hero from "../components/Hero"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Testimonials = () => {
   return (
     <>
       <Layout>
-        <div>Rekomendacje</div>
+        <Hero />
+        <section className="testimonials" id="testimonials">
+          <div className="container mt-3">
+            <h2 className="testimonials-title">rekomendacje</h2>
+            <h3 className="testimonials-subtitle">Co mówią o nas ludzie?</h3>
+            <div className="testimonials-cards">
+              <article className="testimonials-card">
+                <h4 className="testimonials-card-title">
+                  Szkoła Podstawowa im. Kawalerów Orderu Uśmiechu w Józefinie
+                </h4>
+                <p className="testimonials-card-description">
+                  Dyrektor, Grono Pedagogiczne oraz Uczniowie Szkoła Podstawowa
+                  im. Kawalerów Orderu Uśmiechu w Józefinie serdecznie dziękują
+                  panu Jerzemu Wiśniewskiemu
+                </p>
+                <span className="testimonials-card-name">
+                  Marzena Pytlak - dyrektor szkoły
+                </span>
+                <div className="testimonials-card-img-wrapper">
+                  <StaticImage
+                    src="../assets/img/referencja-3.jpg"
+                    alt="zdjęcie referencji klienta"
+                    layout="constrained"
+                    placeholder="tracedSVG"
+                  />
+                </div>
+              </article>
+              <article className="testimonials-card">
+                <h4 className="testimonials-card-title">
+                  Zespół FRESH z UKS "Olimpijczyk 2008
+                </h4>
+                <p className="testimonials-card-description">
+                  Dziękuje przyjaciołom zespołu za wsparcie w wyjeździe na XVI
+                  MPCh 2013
+                </p>
+                <span className="testimonials-card-name">
+                  Zespół FRESH z UKS "Olimpijczyk 2008
+                </span>
+                <div className="testimonials-card-img-wrapper">
+                  <StaticImage
+                    src="../assets/img/referencja-4.jpg"
+                    alt="zdjęcie referencji klienta"
+                    layout="constrained"
+                    placeholder="tracedSVG"
+                  />
+                </div>
+              </article>
+              <article className="testimonials-card">
+                <h4 className="testimonials-card-title">
+                  Dom Pracy Twórczej w Radziejowice
+                </h4>
+                <p className="testimonials-card-description">
+                  Proszę przyjąć moje serdeczne podziękowania za dotychczasową
+                  współpracę. Chciałbym pogratulować Panu świadczenia usług
+                  transportowych na najwyższym poziomie. Pana oraz Pańscy
+                  Współpracownicy dali się poznać jako zgrany i profesjonalny
+                  zespół. Pana zaangażowanie, życzliwość, elastyczność,
+                  sprawiają, że zdobywamy uznanie w oczach naszych gości. To
+                  również dzięki Pana firmie Pałac w Radziejowicach postrzegany
+                  jest jako miejsce z obsługą najwyższej jakości. Licząc na
+                  kolejne lata tak owocnej współpracy, przesyłam wyrazy
+                  szacunku.
+                </p>
+                <span className="testimonials-card-name">
+                  Bogumił Mrówczyński - dyrektor
+                </span>
+                <div className="testimonials-card-img-wrapper">
+                  <StaticImage
+                    src="../assets/img/referencja-1.jpg"
+                    alt="zdjęcie referencji klienta"
+                    layout="constrained"
+                    placeholder="tracedSVG"
+                  />
+                </div>
+              </article>
+            </div>
+          </div>
+        </section>
       </Layout>
     </>
   )
