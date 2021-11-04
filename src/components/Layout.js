44b506b99d41d31d1611f9c413659e5e92c67d36
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-11-04 22:15:01 +0100

    add page transitions to the Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,13 +3,19 @@ import Navbar from "./Navbar"
 import ActionNav from "./ActionNav"
 import Footer from "./Footer"
 import "../sass/main.scss"
+import { motion } from "framer-motion"
 
 const Layout = ({ children }) => {
   return (
     <>
       <ActionNav />
       <Navbar />
-      <main className="main">{children}</main>
+      <motion.div
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0, transition: { duration: 0.7 } }}
+      >
+        <main className="main">{children}</main>
+      </motion.div>
       <Footer />
     </>
   )
