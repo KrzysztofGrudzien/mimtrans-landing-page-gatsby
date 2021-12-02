b0a3c96c40843818a32f74fed51cad0a83d4b448
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-12-02 18:51:02 +0100

    create a mobile navigation and style it

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import data from "../data/data"
 import { AiTwotoneCalendar } from "@react-icons/all-files/ai/AiTwotoneCalendar"
 import { FaMoneyCheckAlt } from "@react-icons/all-files/fa/FaMoneyCheckAlt"
+import { IoIosMenu } from "@react-icons/all-files/io/IoIosMenu"
 import logo from "../assets/img/logo.svg"
 
 const Navbar = () => {
@@ -27,7 +28,41 @@ const Navbar = () => {
       <ImgWrapper>
         <img src={logo} alt="logo" />
       </ImgWrapper>
-      <nav className="navbar">
+      <nav className="navbar navbar-desktop">
+        <ul className="navbar-list">
+          {navigation.map(
+            (
+              {
+                className,
+                path,
+                title,
+                linkClassName,
+                icon,
+                iconFill,
+                iconSize,
+              },
+              index
+            ) => {
+              return (
+                <li className={className} key={index}>
+                  <Link
+                    to={path}
+                    className={linkClassName}
+                    activeClassName="navbar-link--active"
+                  >
+                    {addIcon(icon, iconFill, iconSize)}
+                    {title}
+                  </Link>
+                </li>
+              )
+            }
+          )}
+        </ul>
+      </nav>
+      <nav className="navbar navbar-mobile">
+        <MenuIconBox>
+          <IoIosMenu size="40" />
+        </MenuIconBox>
         <ul className="navbar-list">
           {navigation.map(
             (
@@ -71,6 +106,16 @@ const Header = styled.header`
     align-items: center;
     display: flex;
   }
+`
+
+const MenuIconBox = styled.div`
+  position: fixed;
+  top: 2rem;
+  right: 2rem;
+  width: 2rem;
+  height: 2rem;
+  cursor: pointer;
+  z-index: 2000;
 `
 
 const ImgWrapper = styled.div`
--- src/pages/aktualnosci.js
+++ src/pages/aktualnosci.js
@@ -16,14 +16,6 @@ const News = () => {
           </div>
           <ul className="news-list">
             <li className="news-item">
-              <b>15.11.2021</b>
-              <span>
-                Przedsiębiorca uzyskał subwencję finansową w ramach programu
-                rządowego „Tarcza Finansowa 2.0 Polskiego Funduszu Rozwoju dla
-                Mikro, Małych i Średnich Firm”, udzieloną przez PFR SA.
-              </span>
-            </li>
-            <li className="news-item">
               <b>17.09.2021</b>
               <span>
                 Kontakt mailowy po przez stronę mimtrans.pl został uruchomiony.
